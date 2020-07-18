d1a7205376e3a94aa10bd1a078593b55b955e4b2
Mert <mertturkmenoglu99@gmail.com>
2020-07-18 22:52:28 +0300

    Initial commit

---
+++ src/test/kotlin/io/github/mertturkmenoglu/testapi/TestapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.mertturkmenoglu.testapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
