66678d1421f9e354653b3b964023aa90a04ee538
Mert <mertturkmenoglu99@gmail.com>
2020-07-18 22:57:39 +0300

    Add controller

---
+++ src/main/kotlin/io/github/mertturkmenoglu/testapi/Greeting.kt
@@ -0,0 +1,3 @@
+package io.github.mertturkmenoglu.testapi
+
+data class Greeting(val id: Long, val content: String)
---
+++ src/main/kotlin/io/github/mertturkmenoglu/testapi/GreetingController.kt
@@ -0,0 +1,17 @@
+package io.github.mertturkmenoglu.testapi
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class GreetingController {
+    val template = "Hello, %s!"
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
+        return Greeting(counter.incrementAndGet(), String.format(template, name))
+    }
+}
